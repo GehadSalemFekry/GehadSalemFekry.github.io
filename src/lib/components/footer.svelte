ca3650f9331d96d690222d55b396821c43d57da6
Gehad Salem <gehad.20334@stemalex.moe.edu.eg>
2022-01-18 05:19:41 +0200

    Fixed Footer

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,29 +1,29 @@
 <script>
-  import FacebookIcon from './facebook-icon.svelte'
-  import TwitterIcon from './twitter-icon.svelte'
-  import YouTubeIcon from './you-tube-icon.svelte'
+  import {
+    Facebook,
+    Linkedin,
+    Github,
+  } from "svelte-bootstrap-icons";
 </script>
+<!-- inset-x-0 bottom-0 fixed -->
 
-<footer
-  class="p-10 footer bg-primary text-primary-content footer-center"
->
-  <div>
-    <div class="grid grid-flow-col gap-4">
-      <a href="https://twitter.com">
-        <TwitterIcon />
-      </a>
-      <a href="https://youtube.com">
-        <YouTubeIcon />
-      </a>
-      <a href="https://facebook.com">
-        <FacebookIcon />
-      </a>
-    </div>
-  </div>
+<footer class="p-6 footer bg-primary text-primary-content">
   <div>
     <p>
-      Copyright &copy; {`${new Date().getFullYear()}`} - All right reserved
-      by ME
+      &copy {`${new Date().getFullYear()}`} Copyright - All right reserved by ME 
     </p>
   </div>
+
+  <div class="grid grid-flow-col gap-8 col-end-12">
+    <a href="https://www.facebook.com/gehadsalemfekry/">
+      <Facebook />
+    </a>
+    <a href="https://www.linkedin.com/in/gehad-salem/">
+      <Linkedin />
+    </a>
+    <a href="https://github.com/gehadsalemfekry/">
+      <Github />
+    </a>
+  </div>
+  
 </footer>
